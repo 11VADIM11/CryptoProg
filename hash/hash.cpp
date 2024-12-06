81387aef795eea34954b1d54fb5477059ddbacd2
11VADIM11 <vk5736072@gmail.com>
2024-12-06 22:47:02 +0300

    Практика 4

---
+++ hash/hash.cpp
@@ -0,0 +1,49 @@
+#include <cryptopp/sha.h>
+#include <cryptopp/hex.h>
+#include <cryptopp/files.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
+        return 1;
+    }
+
+    std::string filename = argv[1];
+    std::string file_content;
+    std::string hash;
+
+    
+    std::ifstream file(filename, std::ios::binary);
+    if (file) {
+        std::ostringstream ss;
+        ss << file.rdbuf(); 
+        file_content = ss.str();
+        file.close();
+    } else {
+        std::cerr << "Error reading file: " << filename << std::endl;
+        return 1;
+    }
+
+    try {
+        
+        CryptoPP::SHA256 sha;
+        CryptoPP::StringSource(file_content, true,
+            new CryptoPP::HashFilter(sha,
+                new CryptoPP::HexEncoder(
+                    new CryptoPP::StringSink(hash), false 
+                )
+            )
+        );
+
+        std::cout << "Hash: " << hash << std::endl;
+    } catch (const CryptoPP::Exception& e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
