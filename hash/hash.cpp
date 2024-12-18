6b62b3f4bd91caa6f144ff7612d18ef4d5837578
11VADIM11 <vk5736072@gmail.com>
2024-12-18 13:02:44 +0300

    Update hash.cpp

--- hash/hash.cpp
+++ hash/hash.cpp
@@ -8,24 +8,24 @@ using namespace CryptoPP;
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    // Проверка наличия аргумента командной строки
+    
     if (argc != 2) {
         cerr << "Использование: " << argv[0] << " <путь к файлу>" << endl;
         return 1;
     }
 
-    // Путь к файлу для хэширования
+    
     const char *filename = argv[1];
 
-    // Переменные для хранения хэша
-    byte hash[Weak::MD5::DIGESTSIZE]; // Массив для хранения значений хэша
+    
+    byte hash[Weak::MD5::DIGESTSIZE]; 
 
-    // Используем FileSource для чтения содержимого файла
+    
     try {
-        // Создание объекта MD5
+        
         Weak::MD5 md5;
 
-        // Чтение файла и обновление хэша
+        
         FileSource fileSource(
             filename, true,
             new HashFilter(md5, new ArraySink(hash, sizeof(hash)))
@@ -35,11 +35,11 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // Кодирование хэша в шестнадцатеричном формате
+    
     string encodedHash;
     StringSource(hash, sizeof(hash), true, new HexEncoder(new StringSink(encodedHash)));
 
-    // Вывод результата
+    
     cout << "Text HASH: " << encodedHash << endl;
 
     return 0;
